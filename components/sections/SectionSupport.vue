ac44f32fe472d667678f1fd4f5176b3db8076efc
Kevin <unfricht.kevin@hotmail.fr>
2020-10-06 11:28:50 +0200

    UPDATE (section support) : translations

--- components/sections/SectionSupport.vue
+++ components/sections/SectionSupport.vue
@@ -20,20 +20,20 @@ export default {
 
             <div class="section-content reduced anim text-center anim-2 mb-5">
                 <h2 class="display-4 display-title anim-2">
-                    Support Hajime, why?
+                    {{ $t("support.title") }}
                 </h2>
                 <div class="title-desc anim-3">
-                    Hajime is an MIT licensed open-source and completely free to use. However, the amount of effort needed to maintain and develop new features for the project is not sustainable without proper financial backing. Currently, more than 600h (~ 4 months of work time) has been invest into this software from his author. You can support Hajime development via the following methods:
+                    {{ $t("support.description", [`600h (~4 ${$tc("time.month", 4)} ${$t("time.work")})`]) }}
                 </div>
             </div>
 
             <div class="btns-action text-center">
                 <a :href="patreon_url" target="_blank" class="btn btn-white btn-round btn-full">
-                    <i class="fab fa-patreon"></i> Patreon (recurring pledge)
+                    <i class="fab fa-patreon"></i> Patreon ({{ $t("support.patreon") }})
                 </a>
 
                 <a :href="paypal_url" target="_blank" class="btn btn-round btn-full">
-                    <i class="fab fa-paypal"></i> Paypal (one time donation)
+                    <i class="fab fa-paypal"></i> Paypal ({{ $t("support.paypal") }})
                 </a>
             </div>
         </div>
